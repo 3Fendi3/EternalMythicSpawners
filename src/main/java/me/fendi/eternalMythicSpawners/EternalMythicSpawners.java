7d1937d476125ec5f5d00e5a539531b6cc9a09b8
fendi <grifnala@gmail.com>
2026-06-04 22:27:07 +0200

    new

---
+++ src/main/java/me/fendi/eternalMythicSpawners/EternalMythicSpawners.java
@@ -0,0 +1,16 @@
+package me.fendi.eternalMythicSpawners;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EternalMythicSpawners extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
